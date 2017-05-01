5715955893e29d35b00a54f9a7b6342433092855
Jennifer Nguyen <jnhallquist@gmail.com>
2017-04-30 23:01:46 -0700

    Style cleanup tabs to spaces

--- src/main/java/com/jennifernguyen/app/Applicant.java
+++ src/main/java/com/jennifernguyen/app/Applicant.java
@@ -4,37 +4,37 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-public class Applicant 
-{
-	private String firstName;
-	private String lastName;
-	private int age;
-	private String applicantState;
-	
-	public Applicant(String firstName, String lastName, int age, String applicantState) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.age = age;
-		this.applicantState = applicantState;
-	}
-	
+public class Applicant {
+
+    private String firstName;
+    private String lastName;
+    private String applicantState;
+    private int age;
+
+    public Applicant(String firstName, String lastName, int age, String applicantState) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.applicantState = applicantState;
+    }
+
     public boolean hasValidName() {
-    	Pattern regex = Pattern.compile("^[A-Z]{1}[a-z]");
-    	Matcher matchFirstName = regex.matcher(firstName);
-    	Matcher matchLastName = regex.matcher(lastName);
-    	
-    	return matchFirstName.find() && matchLastName.find();
+        Pattern regex = Pattern.compile("^[A-Z]{1}[a-z]");
+        Matcher matchFirstName = regex.matcher(firstName);
+        Matcher matchLastName = regex.matcher(lastName);
+
+        return matchFirstName.find() && matchLastName.find();
     }
-    
+
     public boolean hasValidAge() {
-    	return age >= 0;
+        return age >= 0;
     }
-    
-	public int getAge() {
-		return age;
+
+    public int getAge() {
+        return age;
 	}
 
-	public String getApplicantState() {
-		return applicantState;
-	}
+    public String getApplicantState() {
+        return applicantState;
+    }
 }
